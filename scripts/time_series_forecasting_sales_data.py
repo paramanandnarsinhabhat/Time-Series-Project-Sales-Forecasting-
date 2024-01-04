30b3e860028fb02d52910f7d0052389f0ab4a2fd
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-04 17:02:46 +0530

    commit the first few no of rows

--- scripts/time_series_forecasting_sales_data.py
+++ scripts/time_series_forecasting_sales_data.py
@@ -12,3 +12,13 @@ warnings.filterwarnings('ignore')
 
 
 
+# loading the data
+data = pd.read_csv("/Users/paramanandbhat/Downloads/Final_Project/data/Train_KQyJ5eh.csv")
+
+print(data.shape)
+
+#Check the first few number of rows 
+print(data.head())
+
+
+
