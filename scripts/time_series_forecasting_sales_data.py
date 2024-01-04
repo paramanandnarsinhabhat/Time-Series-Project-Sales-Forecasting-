665c616c1a89babc1daed8c8212af42a42375e48
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-04 16:59:22 +0530

    commit scripts

---
+++ scripts/time_series_forecasting_sales_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+# If you are in a Jupyter notebook, use the following line to display plots inline:
+from sklearn.metrics import mean_squared_log_error
+from math import sqrt
+from statistics import mean, stdev
+import warnings
+# To ignore warnings in your script or notebook, you can use:
+warnings.filterwarnings('ignore')
+
+
+
