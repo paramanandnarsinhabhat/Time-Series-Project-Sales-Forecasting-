0d7ed8c306cf6f74ce73cc6a473e98148ca0c824
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-04 17:07:52 +0530

    convert the date column to datetime

--- scripts/time_series_forecasting_sales_data.py
+++ scripts/time_series_forecasting_sales_data.py
@@ -23,6 +23,23 @@ print(data.head())
 #Check the last few no of rows
 print(data.tail())
 
+'''
+- We have daily sales data for two years, starting from 1-jan-07 to 24-dec-08.
+- Using this historical data, we need to forecast the demand expected in the next 6 months. 
 
+Let us do some basic exploration and find out if the given data has any trend or seasonal patterns.
+'''
+
+# 3. Preprocessing the Data
+### Plotting Time Series
+
+data['Date'] = pd.to_datetime(data['Date'], format='%d-%b-%y')
+data.index = data['Date']
+
+plt.figure(figsize=(12,8))
+
+plt.plot(data.index, data['Number_SKU_Sold'], label='Train')
+plt.legend(loc='best')
+plt.show()
 
 
